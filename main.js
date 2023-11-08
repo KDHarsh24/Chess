c68634848337e1537a4ebd50a63772a740333609
Harsh Kumar Das <127042711+KDHarsh24@users.noreply.github.com>
2023-11-08 19:28:34 +0530

    TIll with help on Frontend

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const player = document.querySelector("#player")
 const infoDisplay = document.querySelector("#info-display")
 
 const size = 8
-
+let mover = 'white'
+player.textContent = 'white'
+//Array of 64 for each box in chessboard
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -14,7 +16,7 @@ const startPieces = [
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
-
+//Making Chess board in JS only
 function createChessBoard()
 {
     startPieces.forEach((startPiece, i) => {
@@ -23,11 +25,11 @@ function createChessBoard()
         square.innerHTML = startPiece
         square.setAttribute('square-id', i)
         if((Math.floor(i/8)%2) == 0)
-            square.classList.add(i%2 === 0 ? "beige" : "brown")
+            square.classList.add(i%2 == 0 ? "beige" : "brown")
         else
-            square.classList.add(i%2 === 0 ? "brown" : "beige")
+            square.classList.add(i%2 == 0 ? "brown" : "beige")
 
-        if ( i < 16 )
+        if (i < 16)
             square.firstChild.firstChild.classList.add('black')
         else if(i > 47)
             square.firstChild.firstChild.classList.add('white')
@@ -39,12 +41,74 @@ function createChessBoard()
 }
 createChessBoard();
 
-const allSquares = document.querySelectorAll("#gameboard .square")
+const allSquares = document.querySelectorAll(".square")
 
 allSquares.forEach(square => {
     square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
 })
 
+let startPositionId
+let draggedElement
+let correctMoving = false
+//e.target is giving which div has cursor now
 function dragStart(e){
-    
+    startPositionId = e.target.parentNode.getAttribute('square-id')
+    draggedElement = e.target
+    if(draggedElement.firstChild.classList.contains(mover))
+        correctMoving = true
+    else
+        correctMoving = false
+}
+
+function dragOver(e){
+    if(correctMoving)
+        e.preventDefault()
+    else
+        e.draggable = false
+    //just to make dragging legal
+}
+
+function dragDrop(e){
+    e.stopPropagation()
+    const correct_mover = draggedElement.firstChild.classList.contains(mover)
+    const selectPiece = e.target.classList.contains('piece')
+    console.log("Piece:", selectPiece)
+    console.log("Hand : ", correct_mover)
+    let opponent = 'black'
+    if(mover === 'black')
+        opponent = 'white'
+    const takenByOpponent = e.target.firstChild?.classList.contains(opponent)
+    //e.target.parentNode.append(draggedElement)
+    //e.target.remove()
+    //e.target.append(draggedElement)
+    changePlayer()
+}
+
+function changePlayer(){
+    if(mover === 'black'){
+        mover = 'white'
+        player.textContent = 'white'
+        reverseBoard()
+    }
+    else{
+        mover = 'black'
+        player.textContent = 'black'
+        revertBoard()
+    }
+}
+
+function reverseBoard(){
+    const allSquares = document.querySelectorAll(".square")
+    allSquares.forEach((square, i) =>{
+        square.setAttribute('square-id', (63 - i))
+    })
+}
+
+function revertBoard(){
+    const allSquares = document.querySelectorAll(".square")
+    allSquares.forEach((square, i) =>{
+        square.setAttribute('square-id', i)
+    })
 }
