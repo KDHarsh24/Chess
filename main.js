9865a6ded893acf71a2f4e19492dfb296303953e
Harsh Kumar Das <127042711+KDHarsh24@users.noreply.github.com>
2023-11-13 13:41:26 +0530

    Pawn movement done

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ const startPieces = [
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+//for making moveable spots
+//const dotspot = document.createElement('div');
+//dotspot.classList.add('dotspot')
+
 //Making Chess board in JS only
 function createChessBoard()
 {
@@ -23,12 +28,14 @@ function createChessBoard()
         const square = document.createElement('div');
         square.classList.add('square') //used when adding a class property of css to element
         square.innerHTML = startPiece
-        square.setAttribute('square-id', i)
+        square.setAttribute('square-id', i)//New Attribute to html element
+        //Color on alternate squares
         if((Math.floor(i/8)%2) == 0)
             square.classList.add(i%2 == 0 ? "beige" : "brown")
         else
             square.classList.add(i%2 == 0 ? "brown" : "beige")
 
+        //Strokes color of piece
         if (i < 16)
             square.firstChild.firstChild.classList.add('black')
         else if(i > 47)
@@ -37,11 +44,13 @@ function createChessBoard()
         if (square.firstChild)
             square.firstChild.setAttribute('draggable', true)
         gameboard.append(square)
+
+        reverseBoard()
     })
 }
 createChessBoard();
 
-const allSquares = document.querySelectorAll(".square")
+const allSquares = document.querySelectorAll(".square");
 
 allSquares.forEach(square => {
     square.addEventListener('dragstart', dragStart)
@@ -56,6 +65,7 @@ let correctMoving = false
 function dragStart(e){
     startPositionId = e.target.parentNode.getAttribute('square-id')
     draggedElement = e.target
+    console.log(draggedElement.id, " selected from box no. ",  startPositionId)
     if(draggedElement.firstChild.classList.contains(mover))
         correctMoving = true
     else
@@ -72,9 +82,8 @@ function dragOver(e){
 
 function dragDrop(e){
     e.stopPropagation()
-    const correct_mover = draggedElement.firstChild.classList.contains(mover)
     const selectPiece = e.target.classList.contains('piece')
-    const valid = pieceMoving(e.target)
+    const valid = pieceMoving(e.target, selectPiece)
     let opponent = 'black'
     if(mover === 'black')
         opponent = 'white'
@@ -88,11 +97,12 @@ function dragDrop(e){
         changePlayer()
         return
     }
-    if(selectPiece && takenByOpponent)
+    if(takenByOpponent && valid)
     {
-        console.log(e.target)
+        console.log(e.target.firstChild)
         e.target.firstChild.remove()
-        e.target.append(draggedElement)
+        e.target.parentNode.append(draggedElement)
+        //console.log(e.target.firstChild, e.target)
         changePlayer()
         return
     }
@@ -105,12 +115,12 @@ function changePlayer(){
     if(mover === 'black'){
         mover = 'white'
         player.textContent = 'white'
-        //reverseBoard()
+        reverseBoard()
     }
     else{
         mover = 'black'
         player.textContent = 'black'
-        //revertBoard()
+        revertBoard()
     }
 }
 
@@ -128,11 +138,35 @@ function revertBoard(){
     })
 }
 
-function pieceMoving(target)
+function pieceMoving(target, selectPiece)
 {
-    const targetId = Number(target.getAttribute('square-id')) || Number()
+    const targetId = Number(target.getAttribute('square-id')) || Number(target.parentNode.getAttribute('square-id'))
     const startId = Number(startPositionId)
-    const piece = draggedElement.id 
-    console.log(targetId, startId, piece)
+    const piece = draggedElement.id
+    //console.log(targetId, startId, piece)
+    if(piece === 'pawn')//for pawn movement
+    {
+        //if pawn has something in front but still valid cutting
+        if(document.querySelector(`[square-id = "${startId + size}"]`).firstChild)
+        {
+            //console.log(document.querySelector(`[square-id = "${startId + size}"]`).firstChild, targetId, startId)
+            //target id as in if and selected piece as only possible if opponent
+            if(selectPiece && (targetId === startId+size-1 || targetId === startId+size+1))
+                return true
+            else
+                return false
+        }
+        else
+        {
+            if(
+                (startId > 7 && startId < 16 && targetId == startId+16 && !document.querySelector(`[square-id = "${startId + size*2}"]`).firstChild) ||
+                targetId == startId+8)
+                return true
+            if(selectPiece && (targetId === startId+size-1 || targetId === startId+size+1))
+                return 1;
+            else
+                return false
+        }
+    }
     return 1
 }
